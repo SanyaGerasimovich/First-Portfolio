35373d5206b88c160265ad43fbbb674e904fc8f0
SanyaGerasimovich <102600693+SanyaGerasimovich@users.noreply.github.com>
2022-05-06 02:19:02 +0300

    Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function visibleAbout() {
+    document.getElementById('about_me').style.opacity = '0.9';
+}
+
+function hiddenAbout() {
+    document.getElementById('about_me').style.opacity = '0';
+}
+function visibleProjects() {
+    document.getElementById('projects').style.opacity = '0.9';
+}
+function hiddenProjects() {
+    document.getElementById('projects').style.opacity = '0';
+}
+function visibleContact() {
+    document.getElementById('contact').style.opacity = '0.9';
+}
+function hiddenContact() {
+    document.getElementById('contact').style.opacity = '0';
+}
